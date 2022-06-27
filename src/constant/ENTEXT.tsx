dbbeb389ef2a2e1f277c3dceb2c160bb44e17467
houshanyun <likeredbean@gmail.com>
2022-06-27 15:18:01 +0800

    typing02-ENTEXT

--- src/constant/ENTEXT.tsx
+++ src/constant/ENTEXT.tsx
@@ -1,5 +1,7 @@
-const TEXT: string = `Here you can find activities to practise your reading skills. Reading will help you to improve your understanding of the language and build your vocabulary.
-The self-study lessons in this section are written and organised according to the levels of the Common European Framework of Reference for languages (CEFR). There are different types of texts and interactive exercises that practise the reading skills you need to do well in your studies, to get ahead at work and to communicate in English in your free time.
-Take our free online English test to find out which level to choose. Select your level, from beginner (CEFR level A1) to advanced (CEFR level C1), and improve your reading skills at your own speed, whenever it's convenient for you.`
+const TEXT: string = 
+`(CNN) — With St. Patrick's Day a global phenomenon and Irish pubs found everywhere from Peru to Lanzarote, it can be easy to think you have a sense of Ireland without visiting, especially if you're one of the 70 million people worldwide who can claim Irish heritage.
+However, to get a true feel for the modern energy of this little island nation, you need to visit, and most people start their journey on the streets of Dublin.
+It's a compact, walkable capital city, its low-rise skyline and Georgian granite landmarks built on a human scale.`
+
 
 export const ENTEXT = Array.from(TEXT)
